7fe733603e38531062a829123a04d2db580660d5
chris <chris@layer8.org>
2024-08-19 16:16:10 -0400

    better struct alignment

--- dnsmessage/models.go
+++ dnsmessage/models.go
@@ -3,80 +3,81 @@ package dnsmessage
 import "time"
 
 type DNSMessage struct {
-	Type                    string
-	MessageId               []byte
-	ServerIdentity          string
-	SocketFamily            string
-	SocketProtocol          string
-	From                    string
-	To                      string
-	InBytes                 uint64
-	TimeSec                 uint32
-	TimeUsec                uint32
-	Id                      uint32
-	Question                DNSQuestion
-	Response                DNSResponse
-	OriginalRequestorSubnet []byte
-	RequestorId             string
-	InitialRequestId        []byte
-	DeviceId                []byte
-	NewlyObservedDomain     bool
-	DeviceName              string
-	FromPort                uint32
-	ToPort                  uint32
+	Question                DNSQuestion // Struct
+	Response                DNSResponse // Struct
+	InBytes                 uint64      // 8 bytes
+	TimeSec                 uint32      // 4 bytes
+	TimeUsec                uint32      // 4 bytes
+	Id                      uint32      // 4 bytes
+	FromPort                uint32      // 4 bytes
+	ToPort                  uint32      // 4 bytes
+	Type                    string      // String (pointer)
+	ServerIdentity          string      // String (pointer)
+	SocketFamily            string      // String (pointer)
+	SocketProtocol          string      // String (pointer)
+	From                    string      // String (pointer)
+	To                      string      // String (pointer)
+	RequestorId             string      // String (pointer)
+	DeviceName              string      // String (pointer)
+	NewlyObservedDomain     bool        // 1 byte
+	OriginalRequestorSubnet []byte      // Slice (pointer)
+	InitialRequestId        []byte      // Slice (pointer)
+	DeviceId                []byte      // Slice (pointer)
+	MessageId               []byte      // Slice (pointer)
+
 }
 
 type DNSQuestion struct {
-	QName  string
-	QType  uint32
-	QClass uint32
+	QName  string // String (pointer)
+	QType  uint32 // 4 bytes
+	QClass uint32 // 4 bytes
 }
 
 type DNSResponse struct {
-	Rcode                uint32
-	Rrs                  []DNSResponse_DNSRR
-	AppliedPolicy        string
-	Tags                 []string
-	QueryTimeSec         uint32
-	QueryTimeUsec        uint32
-	AppliedPolicyTrigger string
-	AppliedPolicyHit     string
-	ValidationState      string
+	AppliedPolicy        string              // String (pointer)
+	AppliedPolicyTrigger string              // String (pointer)
+	AppliedPolicyHit     string              // String (pointer)
+	ValidationState      string              // String (pointer)
+	Rrs                  []DNSResponse_DNSRR // Slice (pointer)
+	Tags                 []string            // Slice (pointer)
+	Rcode                uint32              // 4 bytes
+	QueryTimeSec         uint32              // 4 bytes
+	QueryTimeUsec        uint32              // 4 bytes
 }
 
 type DNSResponse_DNSRR struct {
-	Name  string
-	Type  uint32
-	Class uint32
-	Ttl   uint32
-	Rdata string
-	Udr   bool
+	Name  string // String (pointer)
+	Rdata string // String (pointer)
+	Type  uint32 // 4 bytes
+	Class uint32 // 4 bytes
+	Ttl   uint32 // 4 bytes
+	Udr   bool   // 1 byte
 }
 
 type PassiveDNSRecord struct {
-	Id        string
-	Qname     string
-	Rname     string
-	Rtype     uint32
-	Rdata     string
-	Timestamp time.Time
+	Timestamp time.Time // 24 bytes (on 64-bit architecture)
+	Id        string    // String (pointer)
+	Qname     string    // String (pointer)
+	Rname     string    // String (pointer)
+	Rdata     string    // String (pointer)
+	Rtype     uint32    // 4 bytes
 }
 
 type AuthoritativeDNSRecord struct {
-	Id        string
-	Qname     string
-	ServerIp  string
-	Rdata     string
-	Timestamp time.Time
+	Timestamp time.Time // 24 bytes (on 64-bit architecture)
+	Id        string    // String (pointer)
+	Qname     string    // String (pointer)
+	ServerIp  string    // String (pointer)
+	Rdata     string    // String (pointer)
 }
 
 type QueryResponseRecord struct {
-	SocketProtocol string
-	Qname          string
-	Rcode          uint32
-	FromIp         string
-	FromPort       uint32
-	ToIp           string
-	ToPort         uint32
-	Timestamp      time.Time
+	Timestamp      time.Time // 24 bytes (on 64-bit architecture)
+	SocketProtocol string    // String (pointer)
+	Qname          string    // String (pointer)
+	FromIp         string    // String (pointer)
+	ToIp           string    // String (pointer)
+	Rcode          uint32    // 4 bytes
+	FromPort       uint32    // 4 bytes
+	ToPort         uint32    // 4 bytes
 }
